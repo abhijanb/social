82ee28b97b1bd2a677186530e805386f94abbd8d
abhijanb <abhijan097@gmail.com>
2026-09-13 17:03:42 +0545

    refactor: remove unused routes for Login and Profile pages

--- front/src/app/route.tsx
+++ front/src/app/route.tsx
@@ -1,9 +1,4 @@
 import type { RouteObject } from 'react-router-dom'
-import Login from '../pages/Login'
-import Profile from '../pages/Profile'
 
 export const routes: RouteObject[] = [
-  { path: '/', element: <Login /> },
-  { path: 'login', element: <Login /> },
-  { path: 'profile', element: <Profile /> },
 ]
